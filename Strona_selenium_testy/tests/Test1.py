25ac4769b4ad36916e0b03d57139094a844661ce
Rapish <43294418+Rapish@users.noreply.github.com>
2018-10-21 18:28:05 +0200

    commit changes in Test1

--- Strona_selenium_testy/tests/Test1.py
+++ Strona_selenium_testy/tests/Test1.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from Strona_selenium_testy.Page_objects.open_website import Web
 from hamcrest import *
+from Strona_selenium_testy.Page_objects.contact_fields import Contact_fields
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class Test1():
@@ -12,16 +16,24 @@ class Test1():
         get_title = driver.title
         my_title = "Selenium Framework | Practiceform"
 
-        if get_title == my_title:
-            print("Wszedłeś na stronę")
-            assert_that(get_title, equal_to(my_title))
-            screens_url = "C:\\Users\\Grzesiek\\PycharmProjects\\Testowe\\Strona_selenium_testy\\Screens\\Test1_screen.png"
-            driver.get_screenshot_as_file(screens_url)
-        else:
-            ("Coś poszło nie tak")
+        try:
+            cf = Contact_fields(driver)
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH, cf.get_imie())))
+            print("Strona została prawidłowo załadowana")
+            try:
+                assert_that(get_title, equal_to(my_title))
+                screens_url = "C:\\Users\\Grzesiek\\PycharmProjects\\Testowe\\Strona_selenium_testy\\Screens\\Test1_screen.png"
+                driver.get_screenshot_as_file(screens_url)
+                print("Tytuł strony jest prawidłowy")
+            except AssertionError:
+                print(AssertionError)
+        except:
+            print("Nie udało się załadować strony!")
 
-        print("Test1 - passed!!!!")
-        print("Brawo")
+
+
+
+
 
         driver.close()
 
